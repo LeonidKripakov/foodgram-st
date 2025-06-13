7ace12d41a561f3d7c6502b8f7ff21c604bd180c
LeonidKripakov <leonidkripakov@gmail.com>
2025-06-13 21:53:25 +0700

    Установлена библиотека Pillow

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    measurement_unit = models.CharField(max_length=50, verbose_name="Единица измерения")
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} ({self.measurement_unit})"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+    color = models.CharField(max_length=7, unique=True, verbose_name="Цвет (HEX)")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="Слаг")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name="Автор"
+    )
+    name = models.CharField(max_length=200, verbose_name="Название")
+    image = models.ImageField(upload_to='recipes/images/', verbose_name="Картинка")
+    text = models.TextField(verbose_name="Описание")
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name="Ингредиенты"
+    )
+    tags = models.ManyToManyField(Tag, related_name='recipes', verbose_name="Теги")
+    cooking_time = models.PositiveIntegerField(verbose_name="Время приготовления (мин)")
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(verbose_name="Количество")
+
+    class Meta:
+        verbose_name = "Ингредиент в рецепте"
+        verbose_name_plural = "Ингредиенты в рецепте"
+        constraints = [
+            models.UniqueConstraint(fields=['recipe', 'ingredient'], name='unique_recipe_ingredient')
+        ]
+
+    def __str__(self):
+        return f"{self.ingredient.name} — {self.amount} ({self.ingredient.measurement_unit})"
